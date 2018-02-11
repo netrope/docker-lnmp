9fe98eb9b0f1e659cce09c47d61a45fba3f952ee
Rojay <netrope@163.com>
2018-02-11 18:10:13 +0800

    完善

--- conf/php7.2/install.sh
+++ conf/php7.2/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#   1：su -
-#   2：/datadisk/website/docker-lnmp/conf/php7.2/install-php7.2.2.sh
 version="7.2.2"
 
 yum update -y
@@ -13,7 +11,61 @@ tar -xvf php-${version}.tar.gz
 rm -f php-${version}.tar.gz
 cd php-${version}
 
-groupadd -r www && useradd -r -g www -s /bin/false -d /usr/local/php -M php
+groupadd -r www && useradd -r -g www -s /bin/false -d /usr/local/php7 -M www
+
+cd /usr/local/src/php-7.2.2
+./configure --prefix=/usr/local/php7 \
+    --exec-prefix=/usr/local/php7 \
+    --bindir=/usr/local/php7/bin \
+    --sbindir=/usr/local/php7/sbin \
+    --includedir=/usr/local/php7/include \
+    --libdir=/usr/local/php7/lib/php \
+    --mandir=/usr/local/php7/php/man \
+    --with-config-file-path=/usr/local/php7/etc \
+    --with-mysql-sock=/var/lib/mysql/mysql.sock \
+    --with-mhash \
+    --with-openssl \
+    --with-mysqli=shared,mysqlnd \
+    --with-pdo-mysql=shared,mysqlnd \
+    --with-gd \
+    --with-iconv \
+    --with-zlib \
+    --enable-zip \
+    --enable-inline-optimization \
+    --disable-debug \
+    --disable-rpath \
+    --enable-shared \
+    --enable-xml \
+    --enable-bcmath \
+    --enable-shmop \
+    --enable-sysvsem \
+    --enable-mbregex \
+    --enable-mbstring \
+    --enable-ftp \
+    --enable-pcntl \
+    --enable-sockets \
+    --with-xmlrpc \
+    --enable-soap \
+    --without-pear \
+    --with-gettext \
+    --enable-session \
+    --with-curl \
+    --with-jpeg-dir \
+    --with-freetype-dir \
+    --enable-opcache \
+    --enable-fpm \
+    --with-fpm-user=www \
+    --with-fpm-group=www \
+    --without-gdbm \
+    --disable-fileinfo
+
+make && make install
+cd ..
 
 ln -s /usr/local/php7/bin/php /usr/local/bin/php
 ln -s /usr/local/php7/bin/phpize /usr/local/bin/phpize
+
+rm -rf /usr/local/src/php-${version}.tar.gz
+rm -rf /usr/local/src/php-${version}
+
+
